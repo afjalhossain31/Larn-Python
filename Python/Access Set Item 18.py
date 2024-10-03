f89191b351339f2b074f21d392dd68cb12240e72
Abdullah Al Afjal <159076048+abdullahalafjal@users.noreply.github.com>
2024-10-03 22:08:06 +0600

    Create Access Set Item 18.py

---
+++ Python/Access Set Item 18.py
@@ -0,0 +1,14 @@
+my_set = {"apple", "banana", "cherry"}
+
+for item in my_set:
+    print(item)
+# Check if an item exists
+if "banana" in my_set:
+    print("Banana is in the set")
+my_list = list(my_set)
+
+print("First item:", my_list[0])
+
+first_item = next(iter(my_set))
+
+print("First item using iter():", first_item)
