90d887c1d2c6a6472d7f245ad2a73ad525e238dd
Abdullah Al Afjal <159076048+abdullahalafjal@users.noreply.github.com>
2024-10-03 21:39:59 +0600

    Update Dictionary 12.py

--- Python/Dictionary 12.py
+++ Python/Dictionary 12.py
@@ -14,3 +14,24 @@ print(null_dict)
 print(type(dict))
 print(dict["name"])
 print(dict["subject"])
+print("\n\n\n\n\n\n")
+
+
+def top_student(student_scores):
+    # Comprehension : {key: value for key, value in iterable}
+    student_dict = {name: score for name, score in student_scores}
+    # {'Alice': 85, 'Bob': 90, 'Charlie': 78}
+    top_student = max(student_dict, key=student_dict.get)
+
+    return (
+        student_dict,
+        f"Top Student: {top_student} with {student_dict[top_student]} marks",
+    )
+
+
+student_scores = [("Alice", 85), ("Bob", 90), ("Charlie", 80)]
+
+student_data, top_student = top_student(student_scores)
+
+print(student_data)
+print(top_student)
